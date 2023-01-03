c3071a45fc6f9eaeed18d4233f4e3f615fbce442
teh_coderer <me@tehcoderer.com>
2023-01-03 11:58:35 -0500

    init

--- terminal.py
+++ terminal.py
@@ -4,10 +4,10 @@ from openbb_terminal import integration_testing, terminal_controller
 from openbb_terminal.plots_core.backend import BACKEND
 
 if __name__ == "__main__":
-    BACKEND.start()
 
     sent_args = sys.argv[1:]
     if "-t" in sent_args or "--test" in sent_args:
         integration_testing.parse_args_and_run()
     else:
+        BACKEND.start()
         terminal_controller.parse_args_and_run()
